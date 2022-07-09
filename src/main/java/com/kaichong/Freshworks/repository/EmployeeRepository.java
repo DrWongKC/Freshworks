67138f7cc994b0ea82eeb4cd7260ebc6cddddb91
DrWongKC <kai_wong@outlook.com>
2022-07-09 04:39:36 -0400

    Added tutorial I got this project from for future reference. :)

--- src/main/java/com/kaichong/Freshworks/repository/EmployeeRepository.java
+++ src/main/java/com/kaichong/Freshworks/repository/EmployeeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     // all crud database methods
+    // Tutorial site: https://www.javaguides.net/2021/10/spring-boot-crud-rest-api-project-using-IntelliJ-IDEA.html
 }
