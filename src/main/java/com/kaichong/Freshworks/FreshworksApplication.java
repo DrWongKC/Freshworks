2a16fecfcc310da64c4e1212ef4219f658c562ec
DrWongKC <kai_wong@outlook.com>
2022-07-09 04:34:56 -0400

    Completed Freshworks

---
+++ src/main/java/com/kaichong/Freshworks/FreshworksApplication.java
@@ -0,0 +1,13 @@
+package com.kaichong.Freshworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaichong/Freshworks/FreshworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaichong.Freshworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
